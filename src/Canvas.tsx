08bafe6e980bfc4ed25322034b704e5ef14d9fcd
Abadi11 <a.salman2040@gmail.com>
2022-11-18 16:04:02 +0000

    atoms-selectors-atomFamily

--- src/Canvas.tsx
+++ src/Canvas.tsx
@@ -1,17 +1,35 @@
+import {createContext, useState} from 'react'
+import {Element, Rectangle} from './components/Rectangle/Rectangle'
 import {PageContainer} from './PageContainer'
-import {Rectangle} from './components/Rectangle/Rectangle'
 import {Toolbar} from './Toolbar'
+import { atom, useSetRecoilState, useRecoilValue } from 'recoil'
+
+export const selectedElementState = atom<number | null>({
+    key: 'selectedElement',
+    default: null
+})
+
+export const elementsState = atom<number[]>({
+    key:'elements',
+    default:[]
+})
+
 
 function Canvas() {
+    const setSelectedElement = useSetRecoilState(selectedElementState)
+    const elements = useRecoilValue(elementsState)
+
     return (
-        <PageContainer
-            onClick={() => {
-                console.log('Deselect all elements!')
-            }}
-        >
-            <Toolbar />
-            <Rectangle />
-        </PageContainer>
+                <PageContainer
+                    onClick={() => {
+                        setSelectedElement(null)
+                    }}
+                >
+                    <Toolbar />
+                    {elements.map((id) => (
+                        <Rectangle key={id} id={id} />
+                    ))}
+                </PageContainer>
     )
 }
 
---
+++ src/examples/Atoms.tsx
@@ -0,0 +1,40 @@
+import {atom, useRecoilState, useRecoilValue} from 'recoil'
+
+const darkModeAtom = atom({
+    key: 'darkMode',
+    default: false
+})
+
+const DarkModeSwitch = () => {
+    const [darkMode, setDarkMode] = useRecoilState(darkModeAtom)
+    console.log('DarkMode', darkMode)
+    return <input 
+    type="checkbox" 
+    checked={darkMode}
+    onChange={(e)=>setDarkMode(e.currentTarget.checked)}
+    />
+}
+
+const Button = () => {
+    const darkMode = useRecoilValue(darkModeAtom)
+    return (
+    <button 
+        style={{backgroundColor: darkMode?'black':'white', color: darkMode?'white':'black'}}
+    >
+        My UI Button
+        </button>
+        )
+}
+
+export const Atoms = () => {
+  return (
+    <div>
+        <div>
+            <DarkModeSwitch />
+        </div>
+        <div>
+            <Button />
+        </div>
+    </div>
+  )
+}
